226a53448830ba865d17c07d26291dffa251dac6
usany <ckd_qja@naver.com>
2026-03-22 22:12:54 +0900

    fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="(tabs)/shuttle" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)/shuttleSeoul" options={{ headerShown: false }} />
         <Stack.Screen
           name="modal"
           options={{ presentation: "modal", title: "Modal" }}
